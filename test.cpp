bc277fb996ca161dc99290a4ce550887318de1a2
Léo Flaventin Hauchecorne <hl037.prog@gmail.com>
2021-09-24 14:35:58 +0200

    +++ Catch2 tests remove

--- test.cpp
+++ test.cpp
@@ -9,6 +9,8 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 
+namespace test_rangeset{
+
 template <typename T>
 struct data_case_t
 {
@@ -41,8 +43,8 @@ struct data_case_t
   const T* end () const { return &start[size]; }
 };
 
-template <typename T>
-void assert_rangeset_equals(const std::initializer_list<std::pair<T, T> > & expected, const RangeSet<T> & set){
+template <typename T, bool B>
+void assert_rangeset_equals(const std::initializer_list<std::pair<T, T> > & expected, const RangeSet<T, B> & set){
   REQUIRE(expected.size() == set.size());
   auto && it1 = expected.begin(), end1 = expected.end();
   auto && it2 = set.cbegin(), end2 = set.cend();
@@ -52,16 +54,16 @@ void assert_rangeset_equals(const std::initializer_list<std::pair<T, T> > & expe
   }
 }
 
-template <typename T>
-void assert_state(const RangeSet<T> & set){
+template <typename T, bool B>
+void assert_state(const RangeSet<T, B> & set){
   REQUIRE(set.data.size() % 2 == 0 );
   if(set.data.size() % 2){
     return;
   }
   auto && it = set.data.begin(), end = set.data.end();
   while(it != end) {
-    REQUIRE((int) it++->dir == (int) RangeSet<T>::end_point_t::LOWER);
-    REQUIRE((int) it++->dir == (int) RangeSet<T>::end_point_t::UPPER);
+    REQUIRE((int) it++->dir == (int) RangeSet<T, B>::end_point_t::LOWER);
+    REQUIRE((int) it++->dir == (int) RangeSet<T, B>::end_point_t::UPPER);
   }
 }
 
@@ -291,8 +293,67 @@ static param_t merge_touching[] = {
   },
 };
 
-template<typename T>
-void test(RangeSet<T> & set, const data_case_t<param_t> & data_case){
+static param_t keep_touching[] = {
+  //////// KEEP_TOUCHING ////////
+  {
+    "Do not Join begin",
+    {{10, 20}, {30, 40}, {50, 60}, {20, 30}},
+    {{10, 20}, {20, 30}, {30, 40}, {50, 60}},
+  },
+  {
+    "Do not Join end",
+    {{10, 20}, {30, 40}, {50, 60}, {40, 50}},
+    {{10, 20}, {30, 40}, {40, 50}, {50, 60}},
+  },
+  {
+    "Do not Join all",
+    {{10, 20}, {30, 40}, {50, 60}, {20, 50}},
+    {{10, 20}, {20, 50}, {50, 60}},
+  },
+  {
+    "Do not Join more start, begin",
+    {{10, 20}, {30, 40}, {50, 60}, {9, 30}},
+    {{ 9, 30}, {30, 40}, {50, 60}},
+  },
+  {
+    "Do not Join more start, end",
+    {{10, 20}, {30, 40}, {50, 60}, {29, 50}},
+    {{10, 20}, {29, 50}, {50, 60}},
+  },
+  {
+    "Do not Join more end, begin",
+    {{10, 20}, {30, 40}, {50, 60}, {20, 41}},
+    {{10, 20}, {20, 41}, {50, 60}},
+  },
+  {
+    "Do not Join more end, end",
+    {{10, 20}, {30, 40}, {50, 60}, {40, 61}},
+    {{10, 20}, {30, 40}, {40, 61}},
+  },
+  {
+    "Join end points, begin",
+    {{10, 20}, {30, 40}, {50, 60}, {10, 40}},
+    {{10, 40}, {50, 60}},
+  },
+  {
+    "Join end points, end",
+    {{10, 20}, {30, 40}, {50, 60}, {30, 60}},
+    {{10, 20}, {30, 60}},
+  },
+  {
+    "Join more both, begin",
+    {{10, 20}, {30, 40}, {50, 60}, {9, 41}},
+    {{9, 41}, {50, 60}},
+  },
+  {
+    "Join more both, end",
+    {{10, 20}, {30, 40}, {50, 60}, {29, 61}},
+    {{10, 20}, {29, 61}},
+  },
+};
+
+template<typename T, bool B>
+void test(RangeSet<T, B> & set, const data_case_t<param_t> & data_case){
   for(auto && param:data_case){
     SECTION(param.name){
       for(auto && p:param.inserted){
@@ -306,10 +367,515 @@ void test(RangeSet<T> & set, const data_case_t<param_t> & data_case){
 
 }
 
+namespace remove {
+
+struct param_t {
+  const char * name;
+  const std::initializer_list<std::pair<int, int> > inserted;
+  const std::initializer_list<std::pair<int, int> > removed;
+  const std::initializer_list<std::pair<int, int> > expected;
+};
+
+static param_t trivial[] = {
+  {
+    "Remove nothing to nothing is still nothing",
+    {},
+    {},
+    {},
+  },
+  {
+    "Remove anything to nothing is still nothing",
+    {},
+    {{10, 20}},
+    {},
+  },
+  {
+    "Remove multiple anything to nothing is still nothing",
+    {},
+    {{10, 20}, {20, 30}},
+    {},
+  },
+};
+
+static param_t one_range[] = {
+  {
+    "Remove before",
+    {{10, 20}},
+    {{2, 8}},
+    {{10, 20}},
+  },
+  {
+    "Remove after",
+    {{10, 20}},
+    {{25, 30}},
+    {{10, 20}},
+  },
+  {
+    "Remove touching before",
+    {{10, 20}},
+    {{8, 10}},
+    {{10, 20}},
+  },
+  {
+    "Remove touching after",
+    {{10, 20}},
+    {{20, 22}},
+    {{10, 20}},
+  },
+  {
+    "Remove middle",
+    {{10, 20}},
+    {{12, 18}},
+    {{10, 12}, {18, 20}},
+  },
+  {
+    "Remove overlap before",
+    {{10, 20}},
+    {{5, 12}},
+    {{12, 20}},
+  },
+  {
+    "Remove overlap after",
+    {{10, 20}},
+    {{18, 25}},
+    {{10, 18}},
+  },
+  {
+    "Remove exact begin",
+    {{10, 20}},
+    {{10, 12}},
+    {{12, 20}},
+  },
+  {
+    "Remove exact end",
+    {{10, 20}},
+    {{18, 20}},
+    {{10, 18}},
+  },
+  {
+    "Remove exact",
+    {{10, 20}},
+    {{10, 20}},
+    {},
+  },
+  {
+    "Remove contained",
+    {{10, 20}},
+    {{8, 22}},
+    {},
+  },
+};
+
+static param_t three_ranges[] = {
+  {
+    "Remove begin overlap",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{8, 22}},
+    {{30, 40}, {50, 60}},
+  },
+  {
+    "Remove begin exact",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{10, 20}},
+    {{30, 40}, {50, 60}},
+  },
+  {
+    "Remove end exact",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{50, 60}},
+    {{10, 20}, {30, 40}},
+  },
+  {
+    "Remove middle overlap",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{28, 42}},
+    {{10, 20}, {50, 60}},
+  },
+  {
+    "Remove end overlap",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{48, 62}},
+    {{10, 20}, {30, 40}},
+  },
+  {
+    "Remove middle begin",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{22, 28}},
+    {{10, 20}, {30, 40}, {50, 60}},
+  },
+  {
+    "Remove middle end",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{42, 48}},
+    {{10, 20}, {30, 40}, {50, 60}},
+  },
+  {
+    "Remove middle touching before, begin",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{20, 28}},
+    {{10, 20}, {30, 40}, {50, 60}},
+  },
+  {
+    "Remove middle touching after, begin",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{22, 30}},
+    {{10, 20}, {30, 40}, {50, 60}},
+  },
+  {
+    "Remove middle touching both, begin",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{20, 30}},
+    {{10, 20}, {30, 40}, {50, 60}},
+  },
+  {
+    "Remove middle touching before, end",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{40, 48}},
+    {{10, 20}, {30, 40}, {50, 60}},
+  },
+  {
+    "Remove middle touching after, end",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{42, 50}},
+    {{10, 20}, {30, 40}, {50, 60}},
+  },
+  {
+    "Remove middle touching both, end",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{40, 50}},
+    {{10, 20}, {30, 40}, {50, 60}},
+  },
+  {
+    "Remove middle overlap before, begin",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{18, 28}},
+    {{10, 18}, {30, 40}, {50, 60}},
+  },
+  {
+    "Remove middle overlap after, begin",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{22, 32}},
+    {{10, 20}, {32, 40}, {50, 60}},
+  },
+  {
+    "Remove middle overlap both, begin",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{18, 32}},
+    {{10, 18}, {32, 40}, {50, 60}},
+  },
+  {
+    "Remove middle overlap before, end",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{38, 48}},
+    {{10, 20}, {30, 38}, {50, 60}},
+  },
+  {
+    "Remove middle overlap after, end",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{42, 52}},
+    {{10, 20}, {30, 40}, {52, 60}},
+  },
+  {
+    "Remove middle overlap both, end",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{38, 52}},
+    {{10, 20}, {30, 38}, {52, 60}},
+  },
+  // REMOVE MIDDLE
+  {
+    "Remove middle exact",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{30, 40}},
+    {{10, 20}, {50, 60}},
+  },
+  {
+    "Remove middle exact overlap",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{30, 42}},
+    {{10, 20}, {50, 60}},
+  },
+  {
+    "Remove middle overlap exact",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{28, 40}},
+    {{10, 20}, {50, 60}},
+  },
+  {
+    "Remove middle overlap overlap",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{28, 42}},
+    {{10, 20}, {50, 60}},
+  },
+  // REMOVE MIDDLE BEGIN
+  {
+    "Remove middle exact, exact previous",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{20, 40}},
+    {{10, 20}, {50, 60}},
+  },
+  {
+    "Remove middle overlap, exact previous",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{20, 42}},
+    {{10, 20}, {50, 60}},
+  },
+  {
+    "Remove middle exact, overlap previous",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{18, 40}},
+    {{10, 18}, {50, 60}},
+  },
+  {
+    "Remove middle overlap, overlap previous",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{18, 42}},
+    {{10, 18}, {50, 60}},
+  },
+  // REMOVE MIDDLE END
+  {
+    "Remove middle exact, exact next",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{30, 50}},
+    {{10, 20}, {50, 60}},
+  },
+  {
+    "Remove middle overlap, exact next",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{28, 50}},
+    {{10, 20}, {50, 60}},
+  },
+  {
+    "Remove middle exact, overlap next",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{30, 52}},
+    {{10, 20}, {52, 60}},
+  },
+  {
+    "Remove middle overlap, overlap next",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{28, 52}},
+    {{10, 20}, {52, 60}},
+  },
+  // REMOVE MIDDLE BOTH
+  {
+    "Remove middle exact previous, exact next",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{20, 50}},
+    {{10, 20}, {50, 60}},
+  },
+  {
+    "Remove middle overlap previous, exact next",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{18, 50}},
+    {{10, 18}, {50, 60}},
+  },
+  {
+    "Remove middle exact previous, overlap next",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{20, 52}},
+    {{10, 20}, {52, 60}},
+  },
+  {
+    "Remove middle overlap previous, overlap next",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{18, 52}},
+    {{10, 18}, {52, 60}},
+  },
+  {
+    "Remove all exact exact",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{10, 60}},
+    {},
+  },
+  {
+    "Remove all overlap exact",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{8, 60}},
+    {},
+  },
+  {
+    "Remove all exact overlap",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{10, 62}},
+    {},
+  },
+  {
+    "Remove all overlap overlap",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{8, 62}},
+    {},
+  },
+  // REMOVE BEGIN MIDDLE
+  {
+    "Remove begin exact, exact after",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{10, 30}},
+    {{30, 40}, {50, 60}},
+  },
+  {
+    "Remove begin overlap, exact after",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{8, 30}},
+    {{30, 40}, {50, 60}},
+  },
+  {
+    "Remove begin exact, overlap after",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{10, 32}},
+    {{32, 40}, {50, 60}},
+  },
+  {
+    "Remove begin overlap, overlap after",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{8, 32}},
+    {{32, 40}, {50, 60}},
+  },
+  {
+    "Remove begin middle exact, exact after",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{10, 40}},
+    {{50, 60}},
+  },
+  {
+    "Remove begin middle overlap, exact after",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{8, 40}},
+    {{50, 60}},
+  },
+  {
+    "Remove begin middle exact, overlap after",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{10, 42}},
+    {{50, 60}},
+  },
+  {
+    "Remove begin middle overlap, overlap after",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{8, 42}},
+    {{50, 60}},
+  },
+  {
+    "Remove begin middle exact, exact next",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{10, 50}},
+    {{50, 60}},
+  },
+  {
+    "Remove begin middle overlap, exact next",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{8, 50}},
+    {{50, 60}},
+  },
+  {
+    "Remove begin middle exact, overlap next",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{10, 52}},
+    {{52, 60}},
+  },
+  {
+    "Remove begin middle overlap, overlap next",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{8, 52}},
+    {{52, 60}},
+  },
+  // REMOVE END MIDDLE
+  {
+    "Remove end exact, exact before",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{40, 60}},
+    {{10, 20}, {30, 40}},
+  },
+  {
+    "Remove end overlap, exact before",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{40, 62}},
+    {{10, 20}, {30, 40}},
+  },
+  {
+    "Remove end exact, overlap before",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{38, 60}},
+    {{10, 20}, {30, 38}},
+  },
+  {
+    "Remove end overlap, overlap before",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{38, 62}},
+    {{10, 20}, {30, 38}},
+  },
+  {
+    "Remove end middle exact, exact before",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{30, 60}},
+    {{10, 20}},
+  },
+  {
+    "Remove end middle overlap, exact before",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{30, 62}},
+    {{10, 20}},
+  },
+  {
+    "Remove end middle exact, overlap before",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{28, 60}},
+    {{10, 20}},
+  },
+  {
+    "Remove end middle overlap, overlap before",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{28, 62}},
+    {{10, 20}},
+  },
+  {
+    "Remove end middle exact, exact previous",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{20, 60}},
+    {{10, 20}},
+  },
+  {
+    "Remove end middle overlap, exact previous",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{20, 62}},
+    {{10, 20}},
+  },
+  {
+    "Remove end middle exact, overlap previous",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{18, 60}},
+    {{10, 18}},
+  },
+  {
+    "Remove end middle overlap, overlap previous",
+    {{10, 20}, {30, 40}, {50, 60}},
+    {{18, 62}},
+    {{10, 18}},
+  },
+};
+
+template<typename T, bool B>
+void test(RangeSet<T, B> & set, const data_case_t<param_t> & data_case){
+  for(auto && param:data_case){
+    SECTION(param.name){
+      for(auto && p:param.inserted){
+        set.insert(p);
+      }
+      for(auto && p:param.removed){
+        set.remove(p);
+      }
+      assert_state(set);
+      assert_rangeset_equals(param.expected, set);
+    }
+  }
+}
+
+}
+
 TEST_CASE("rangeset merge touching"){
-  using namespace insert;
   RangeSet<int> set;
   SECTION("insert"){
+    // Tests insert(pair), insert(start, end), const_iterator, cbegin, cend, size, 
+    using namespace insert;
     data_case_t<param_t> data;
     SECTION("simple"){
       data = simple;
@@ -317,11 +883,66 @@ TEST_CASE("rangeset merge touching"){
     }
     SECTION("common"){
       data = common;
-      test(set,data);
+      test(set, data);
     }
     SECTION("merge_touching"){
       data = merge_touching;
-      test(set,data);
+      test(set, data);
+    }
+  }
+  SECTION("remove"){
+    // Tests remove(pair), remove(start, end) 
+    using namespace remove;
+    data_case_t<param_t> data;
+    SECTION("trivial"){
+      data = trivial;
+      test(set, data);
+    }
+    SECTION("1_range"){
+      data = one_range;
+      test(set, data);
+    }
+    SECTION("3_ranges"){
+      data = three_ranges;
+      test(set, data);
+    }
+  }
+}
+
+TEST_CASE("rangeset keep touching"){
+  RangeSet<int, false> set;
+  SECTION("insert"){
+    // Tests insert(pair), insert(start, end), const_iterator, cbegin, cend, size, 
+    using namespace insert;
+    data_case_t<param_t> data;
+    SECTION("simple"){
+      data = simple;
+      test(set, data);
+    }
+    SECTION("common"){
+      data = common;
+      test(set, data);
+    }
+    SECTION("keep_touching"){
+      data = keep_touching;
+      test(set, data);
+    }
+  }
+  SECTION("remove"){
+    // Tests remove(pair), remove(start, end) 
+    using namespace remove;
+    data_case_t<param_t> data;
+    SECTION("trivial"){
+      data = trivial;
+      test(set, data);
+    }
+    SECTION("1_range"){
+      data = one_range;
+      test(set, data);
+    }
+    SECTION("3_range"){
+      data = three_ranges;
+      test(set, data);
     }
   }
 }
@@ -546,4 +1167,6 @@ TEST_CASE("rangeset merge touching delete2"){
     set.remove(0, 300);
     assert_state(set);
     assert_rangeset_equals({}, set);
-  }
+}
+
+}
